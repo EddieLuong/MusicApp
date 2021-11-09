2dfca08b810b7de1af8a7c8eacca0e576fc35e59
O-pus <hongluong296@gmail.com>
2021-11-09 16:55:53 +0700

    update background and more

---
+++ main.js
@@ -0,0 +1,10 @@
+const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+
+const songs = $$('.song');
+const dashboard = $('.dashboard');
+const playlist = $('.music_player')
+
+// console.log(songs[0].getBoundingClientRect().bottom)
+// console.log()
+
